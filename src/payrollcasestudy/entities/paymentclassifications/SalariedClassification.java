b1402316f80a7823fddafb899fa15bdb19c8d5de
FreemanZhang <sjzhang1901@yahoo.com>
2016-07-25 12:38:51 -0700

    define paymentclassification class for salaried employees

---
+++ src/payrollcasestudy/entities/paymentclassifications/SalariedClassification.java
@@ -0,0 +1,8 @@
+package payrollcasestudy.entities.paymentclassifications;
+
+/**
+ * Created by zhangshijie on 7/22/16.
+ */
+public class SalariedClassification extends PaymentClassification {
+    private double salary;
+}
