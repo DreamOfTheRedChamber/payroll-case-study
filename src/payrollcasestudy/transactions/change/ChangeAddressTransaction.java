941bc17c5d4cf0ee4f27d16d0a8f7dd1952420d3
FreemanZhang <sjzhang1901@yahoo.com>
2016-07-26 11:34:53 -0700

    ChangeName and Address transaction

---
+++ src/payrollcasestudy/transactions/change/ChangeAddressTransaction.java
@@ -0,0 +1,21 @@
+package payrollcasestudy.transactions.change;
+
+import payrollcasestudy.entities.Employee;
+
+/**
+ * Created by zhangshijie on 7/26/16.
+ */
+public class ChangeAddressTransaction extends ChangeEmployeeTransaction {
+
+    private String newAddress;
+
+    public ChangeAddressTransaction( int employeeId, String newAddress ) {
+        super( employeeId );
+        this.newAddress = newAddress;
+    }
+
+    @Override
+    public void changeEmployee(Employee employee) {
+        employee.setAddress( this.newAddress );
+    }
+}
