401abecbd6c8c3bc05d59dacc6e9cd4e9ae34429
FreemanZhang <sjzhang1901@yahoo.com>
2016-07-25 22:50:56 -0700

    implement addCommissionedEmployeeTransaction which indicates that employees are paid on a flat salary plus a commission based on their sales biweekly

---
+++ src/payrollcasestudy/transactions/AddCommissionedEmployeeTransaction.java
@@ -0,0 +1,33 @@
+package payrollcasestudy.transactions;
+
+import payrollcasestudy.entities.paymentclassifications.CommissionedClassification;
+import payrollcasestudy.entities.paymentclassifications.PaymentClassification;
+import payrollcasestudy.entities.paymentmethods.PaymentMethod;
+import payrollcasestudy.entities.paymentschedule.BiWeeklyPaymentSchedule;
+import payrollcasestudy.entities.paymentschedule.PaymentSchedule;
+
+/**
+ * Created by zhangshijie on 7/25/16.
+ */
+public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
+
+    private double monthlySalary;
+    private double commissionRate;
+
+    public AddCommissionedEmployeeTransaction(int employeeId, String employeeName, String employeeAddress, PaymentMethod paymentMethod,
+                                              double monthlySalary, double commissionRate ) {
+        super(employeeId, employeeName, employeeAddress, paymentMethod);
+        this.monthlySalary = monthlySalary;
+        this.commissionRate = commissionRate;
+    }
+
+    @Override
+    protected PaymentSchedule getPaymentSchedule() {
+        return new BiWeeklyPaymentSchedule();
+    }
+
+    @Override
+    protected PaymentClassification getPaymentClassification() {
+        return new CommissionedClassification();
+    }
+}
