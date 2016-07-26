d8cd7460c3acae11d94f027f4872e72ad6c98d7d
FreemanZhang <sjzhang1901@yahoo.com>
2016-07-25 22:44:47 -0700

    implement addSalariedEmployeeTransaction which indicates that employees are paid on a flat salary monthly

---
+++ src/payrollcasestudy/transactions/AddSalariedEmployeeTransaction.java
@@ -0,0 +1,32 @@
+package payrollcasestudy.transactions;
+
+import payrollcasestudy.entities.paymentclassifications.PaymentClassification;
+import payrollcasestudy.entities.paymentclassifications.SalariedClassification;
+import payrollcasestudy.entities.paymentmethods.HoldMethod;
+import payrollcasestudy.entities.paymentschedule.MonthlyPaymentSchedule;
+import payrollcasestudy.entities.paymentschedule.PaymentSchedule;
+
+/**
+ * Created by zhangshijie on 7/25/16.
+ */
+public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction {
+
+    private double salary;
+
+    public AddSalariedEmployeeTransaction(int employeeId, String name, String address, double salary, HoldMethod holdMethod )
+    {
+        super( employeeId, name, address, holdMethod );
+        this.salary = salary;
+    }
+
+    @Override
+    protected PaymentSchedule getPaymentSchedule() {
+        return new MonthlyPaymentSchedule();
+    }
+
+    @Override
+    protected PaymentClassification getPaymentClassification() {
+        return new SalariedClassification();
+    }
+
+}
