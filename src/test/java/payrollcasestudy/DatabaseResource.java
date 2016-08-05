032ca83ac6a09cb3d7dbafffec017cebc25f7a9d
Shijie Zhang <szhang@opentext.com>
2016-08-05 10:57:11 -0700

    instantiate the database before testing, delete it after test

---
+++ src/test/java/payrollcasestudy/DatabaseResource.java
@@ -0,0 +1,31 @@
+package payrollcasestudy;
+
+/**
+ * 
+ */
+import org.junit.rules.ExternalResource;
+
+import payrollcasestudy.boundaries.PayrollDatabase;
+
+public class DatabaseResource extends ExternalResource
+{
+
+	protected PayrollDatabase instance;
+
+	@Override
+	public void before( )
+	{
+		instance = PayrollDatabase.globalPayrollDatabase;
+	}
+
+	@Override
+	public void after( )
+	{
+		instance.clear( );
+	}
+
+	public PayrollDatabase getInstance( )
+	{
+		return instance;
+	}
+}
