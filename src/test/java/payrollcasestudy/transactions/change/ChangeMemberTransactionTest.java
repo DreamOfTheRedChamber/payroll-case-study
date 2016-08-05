ba1a83e33eba9157d1458cfd4cc8c6abd5f16d37
Shijie Zhang <szhang@opentext.com>
2016-08-05 14:05:29 -0700

    test which adds employee to an union affiliation

---
+++ src/test/java/payrollcasestudy/transactions/change/ChangeMemberTransactionTest.java
@@ -0,0 +1,56 @@
+
+package payrollcasestudy.transactions.change;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import payrollcasestudy.DatabaseResource;
+import payrollcasestudy.entities.Employee;
+import payrollcasestudy.entities.affiliation.UnionAffiliation;
+import payrollcasestudy.entities.paymentmethods.Hol	dMethod;
+import payrollcasestudy.transactions.add.AddEmployeeTransaction;
+import payrollcasestudy.transactions.add.AddHourlyEmployeeTransaction;
+
+import static payrollcasestudy.TestConstants.*;
+
+/**
+ * 
+ */
+
+public class ChangeMemberTransactionTest
+{
+
+	@Rule
+	public DatabaseResource databaseResource = new DatabaseResource( );
+
+	@Test
+	public void testChangeMemberTransaction( )
+	{
+		int employeeId = 2;
+		int memberId = 7734;
+		AddEmployeeTransaction addEmployeeTransaction = new AddHourlyEmployeeTransaction(
+				employeeId, "Bill", "Home", new HoldMethod( ), 15.25 );
+		addEmployeeTransaction.execute( );
+
+		ChangeMemberTransaction changeMemberTransaction = new ChangeMemberTransaction(
+				employeeId, memberId, 99.42 );
+		changeMemberTransaction.execute( );
+
+		Employee employee = databaseResource.getInstance( )
+				.getEmployee( employeeId );
+		assertThat( employee.getUnionAffiliation( ), is( notNullValue( ) ) );
+
+		UnionAffiliation unionAffiliation = employee.getUnionAffiliation( );
+		assertThat( unionAffiliation.getDues( ),
+				is( closeTo( 99.42, FLOAT_ACCURACY ) ) );
+
+		Employee member = databaseResource.getInstance( )
+				.getUnionMember( memberId );
+		assertThat( member, is( employee ) );
+	}
+}
